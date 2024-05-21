15692052721e413bbedb0c47cc8180f4c1fe878c
gunawanyusuf <145947074+gunawanyusuf@users.noreply.github.com>
2024-05-21 11:36:59 +0800

    Add files via upload

---
+++ pert7.py
@@ -0,0 +1,76 @@
+# tabel kebenaran
+# and or not xor
+
+# NOT
+print("************Logika Not**********")
+x = False
+y = not x
+print("nilai dari x =", x)
+print("nilai dari y=", y)
+
+# and (hanya bernilai benar) 
+print("************Logika And**********")
+x = False
+y = False
+z = x and y
+print(z)
+
+x = False
+y = True
+z = x and y
+print(x, "and", y, "=", z)
+
+x = True
+y = False
+z = x and y 
+print(x, "and", y, "=", z)
+
+x = True
+y = True
+z = x and y
+print(x, "and", y, "=", z)
+
+# or
+print("**************Loguika Or************")
+x = False
+y = False
+z = x and y
+print(x, "or", y, "=", z)
+
+x = True
+y = False
+z = x or y
+print(x, "or", y, "=", z)
+
+x = False
+y = True
+z =x or y
+print(x, "or", y, "=", z)
+
+x = True
+y = True
+z = x or y
+print(x, "or", y, "=", z)
+
+# or
+print("************Logika Or**********")
+x = False
+y = False
+z = x or y
+print(x, "or", y, "=", z)
+
+x = True
+y = False
+z = x or y
+print(x, "or", y, "=", z)
+
+x = False
+y = True
+z = x or y
+print(x, "or", y, "=", z)
+
+x = True
+y = True
+z = x or y
+print(x, "or", y, "=", z)
+
