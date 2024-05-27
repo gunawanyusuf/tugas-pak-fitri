e17ec67aeec1cc3c16e541440b258baca2f77a76
gunawanyusuf <145947074+gunawanyusuf@users.noreply.github.com>
2024-05-27 21:52:50 +0800

    Add files via upload

---
+++ pert2.py
@@ -0,0 +1,13 @@
+# hari ini kita belajar variabel
+# assigment value
+
+x = 3
+y = 9
+print ("Nilai uas sy adalah", y)
+print ("Nilai uas sy adalah", x)
+
+jumlah_a = 100 # tidak boleh spasi
+bungkus10 = 5000 # tidak boleh diawali angka
+
+x = 1000 # assigment kedua 
+print("nilai uas2 sy adalah", x)
---
+++ pert4-5.py
@@ -0,0 +1,37 @@
+# inputan
+# aretmatika + - x :
+
+
+# coba = input("masukan nama :")
+# print("coba")
+
+# bilangan_random = input("masukan bilangan acak")
+# print("bilangan_random")
+
+# aretmatika
+
+x = 9
+y = 3
+
+# penjumlahan 
+hasil = x + y
+print(x,"+", y, "=", hasil)
+
+# perkalian
+hasil = x * y
+print(x, "*", y, "=", hasil)
+
+# pembagian
+hasil = x / y
+print(x, "/", y, "=", hasil)
+
+# floor division //
+hasil = x // y
+print(x, "//", y, "=", hasil)
+
+# skala prioritas (), **, */:, +/-
+x = 3
+y = 4 
+z = 5
+hasil = "(x "+" y * (z + x) / y - z % x // y)"
+print(x, '**', y, '*', z, '+', x, '/', y, '-', z, )
