c9e77be5acb73cc0aafa6f9be7cca9194f41ea24
gunawanyusuf <145947074+gunawanyusuf@users.noreply.github.com>
2024-06-04 11:13:43 +0800

    Update pert3.py

---
+++ pert3.py
@@ -0,0 +1,24 @@
+# BELAJAR TIPE DATA
+
+# TIPE DATA INTIGER : BILANGAN BULAT
+bil_integer = 100
+print (type(bil_integer))
+
+print ("tipe data dari bil_integer adalah :",(type(bil_integer)))
+
+# tipe data float : bilangan desimal
+bil_float = 2.5
+print (type(bil_float))
+print ("tipe data dari bil_float adalah :",(type(bil_float)))
+
+# data string = kumpulan karakter
+bil_string = "30000.s"
+print (type(bil_string))
+"tipe data dari bil_string adalah :", (type(bil_string))
+# tipe data boolean : 0/1, ture/false, ya/tidak
+bil_bol = False
+print (type(bil_bol))
+print ("tipe data bil_bol adalah :", (type(bil_bol)))
+from ctypes import c_double
+bil_double = c_double(30000000)
+print (type(bil_double))
