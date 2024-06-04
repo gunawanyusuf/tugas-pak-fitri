92e806bc5f998ce08a7d8e827d2a845390b5d272
gunawanyusuf <145947074+gunawanyusuf@users.noreply.github.com>
2024-06-04 11:37:55 +0800

    Add files via upload

---
+++ pert8.py
@@ -0,0 +1,21 @@
+# belajar percabangan
+
+nama = input("masukan nama :")
+
+# if kondisi : aksi
+if nama=="edy": print("dosen ganteng")
+print ("program selesai")
+
+# if kondisi : aksi (indentasi)
+if nama=="edy":
+    print("dosen ganteng")
+    print("dosen ganteng")
+print ("program selesai")
+
+
+if nama=="edy":
+    print("dosen ganteng")
+    print(f"ganteng banget,makasi(nama)")
+else: 
+    print("ah gk ganteng")
+print(f"program selesai,thanks (nama)")
