5ba79b2a2705d49b12a201868bc908ed2fe2a57f
gunawanyusuf <145947074+gunawanyusuf@users.noreply.github.com>
2024-06-25 11:51:34 +0800

    Add files via upload

---
+++ pert9.py
@@ -0,0 +1,34 @@
+# belajar perulangan ( loop )
+
+#bil = 1
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+#bil = bil + 2
+#print(bil)
+
+#for kondisi : 
+#   aksi
+
+#pengguna loop pada pemograman lain
+#for (int i; i <=10; i++) {
+#    aksi
+#    aksi
+#}
+
+#perulangan meggunakan list
+bil = [0,8,6,12,19]
+print(bil)
+for i in bil :
+    print(f"nilai i sekarang -> {i}")
+print("ahir pogram i")
+
+#perulangan menggunakan range
+bil2 = range(5)
+for i in bil2 :
+    print(f"nilai i sekarang -> {i}")
+    print("pak edy dosen ganteng")
+
+    print("ahir program 2")
